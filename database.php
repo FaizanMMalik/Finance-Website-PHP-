512f9fb37c23ea0a1091a3a2606f4d9cb4955f23
Faizan Mehmood Malik <115343279+FaizanMMalik@users.noreply.github.com>
2023-07-03 14:30:22 +0500

    Update code

--- database.php
+++ database.php
@@ -1,10 +1,6 @@
 <?php
 
-    $host = "localhost";
-    $user = "Zulifqar";
-    $password = "ahmad123";
-    $dbname = "ksn_db";
-
+  
     // Create connection
     $conn = mysqli_connect($host, $user, $password, $dbname);
 
